0fd12f3c421409589ba33a3f70aaa21154565f7a
sharpiro <dsharpbb09@gmail.com>
2017-10-13 10:52:21 -0400

    adding maths using old stack test project (faster)

---
+++ Discrete.Tests.OldStack/MathTests/BirthdayProblemTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Discrete.Tests.OldStack
+{
+    [TestClass]
+    public class BirthdayProblemTests
+    {
+        [TestMethod]
+        public void BirthdayTest()
+        {
+            var temp = Maths.GetBirthdayProblemChance(200) * 100;
+
+            Assert.AreEqual(0.0, Math.Round(Maths.GetBirthdayProblemChance(1) * 100, 1));
+            Assert.AreEqual(2.7, Math.Round(Maths.GetBirthdayProblemChance(5) * 100, 1));
+            Assert.AreEqual(11.7, Math.Round(Maths.GetBirthdayProblemChance(10) * 100, 1));
+            Assert.AreEqual(41.1, Math.Round(Maths.GetBirthdayProblemChance(20) * 100, 1));
+            Assert.AreEqual(50.7, Math.Round(Maths.GetBirthdayProblemChance(23) * 100, 1));
+            Assert.AreEqual(70.6, Math.Round(Maths.GetBirthdayProblemChance(30) * 100, 1));
+            Assert.AreEqual(89.1, Math.Round(Maths.GetBirthdayProblemChance(40) * 100, 1));
+            Assert.AreEqual(97.0, Math.Round(Maths.GetBirthdayProblemChance(50) * 100, 1));
+            Assert.AreEqual(99.4, Math.Round(Maths.GetBirthdayProblemChance(60) * 100, 1));
+            Assert.AreEqual(99.9, Math.Round(Maths.GetBirthdayProblemChance(70) * 100, 1));
+            Assert.AreEqual(99.99997, Math.Round(Maths.GetBirthdayProblemChance(100) * 100, 5));
+
+            //var temp = Math.Round(Maths.GetBirthdayProblemChance(200) * 100, 28);
+        }
+    }
+}
---
+++ Discrete.Tests.OldStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Discrete.Tests.OldStack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Discrete.Tests.OldStack")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8cce876a-49d5-4451-b5ad-b8ab17d5a5a0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
